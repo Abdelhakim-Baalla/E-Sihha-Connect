adebf39bb5aeab2a9bede1c6328946ec2029ad63
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-10-14 15:39:16 +0100

    Ajoutement de la route /forget-password

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,5 +5,6 @@ const { verifyToken } = require("../middlewares/authMiddleware");
 
 router.post("/inscription", authController.inscription);
 router.post("/connexion", authController.connexion);
+router.post("/forget-password", authController.forgetPassword);
 
 module.exports = router;
