4844d42f4438db75b421e6a110e95cc132239deb
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-10-21 13:59:14 +0100

    Ajout de la vérification des conflits de rendez-vous et mise à jour des méthodes associées

--- src/repositories/RendezVousRepository.js
+++ src/repositories/RendezVousRepository.js
@@ -27,6 +27,26 @@ class RendezVousRepository {
     });
   }
 
+  async checkConflicts(date, duree, medecinId, excludeId = null) {
+    const start = new Date(date);
+    const end = new Date(start.getTime() + duree * 60000);
+    const query = {
+      medecin: medecinId,
+      date: { $lt: end },
+      statut: { $ne: "annulé" },
+    };
+    if (excludeId) {
+      query._id = { $ne: excludeId };
+    }
+    const rdvs = await RendezVous.find(query);
+    
+    return rdvs.filter((rdv) => {
+      const rdvStart = rdv.date;
+      const rdvEnd = new Date(rdv.date.getTime() + rdv.duree * 60000);
+      return start < rdvEnd && end > rdvStart;
+    });
+  }
+
   async findById(id) {
     return await RendezVous.findById(id)
       .populate("patient")
