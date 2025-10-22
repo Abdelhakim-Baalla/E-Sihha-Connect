8444da63c206431e03767fb04a1c10b6c9887d1b
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-10-22 10:39:34 +0100

    Ajout de la documentation Swagger pour les routes d'authentification, de disponibilité, de patients et de rendez-vous

--- src/routes/availabilityRoutes.js
+++ src/routes/availabilityRoutes.js
@@ -4,6 +4,32 @@ const { verifyToken } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /api/v1/availability:
+ *   get:
+ *     summary: Obtenir la disponibilité d'un médecin
+ *     tags: [Availability]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: query
+ *         name: doctorId
+ *         required: true
+ *         schema:
+ *           type: string
+ *       - in: query
+ *         name: date
+ *         required: true
+ *         schema:
+ *           type: string
+ *           format: date
+ *     responses:
+ *       200:
+ *         description: Liste des créneaux libres
+ *       403:
+ *         description: Accès refusé
+ */
 router.get("/", verifyToken, getAvailability);
 
 module.exports = router;
