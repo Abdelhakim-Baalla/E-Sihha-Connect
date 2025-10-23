6d515de39653af708fd7353d748a8f8d273994cf
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2025-10-23 14:26:54 +0100

    Ajout des routes de consultation à l'API E-Sihha Connect

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const authRoutes = require("./routes/authRoutes");
 const patientRoutes = require("./routes/patientRoutes");
 const rendezVousRoutes = require("./routes/rendezVousRoutes");
 const availabilityRoutes = require("./routes/availabilityRoutes");
+const consultationRoutes = require("./routes/consultationRoutes");
 const connectDB = require("./config/db");
 const { seedRoles } = require("./seeders/roleSeeder");
 const { seedUtilisateurs } = require("./seeders/utilisateurSeeder");
@@ -13,6 +14,7 @@ require("./models/MedicalHistorique");
 require("./models/Utilisateur");
 require("./models/Patient");
 require("./models/RendezVous");
+require("./models/Consultation");
 const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("./config/swagger");
 
@@ -26,6 +28,7 @@ app.use("/api/v1/patients", patientRoutes);
 app.use("/api/v1/rendezvous", rendezVousRoutes);
 app.use("/api/v1/rendezvous", bookRendezVousRoutes);
 app.use("/api/v1/availability", availabilityRoutes);
+app.use("/api/v1/consultations", consultationRoutes);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
